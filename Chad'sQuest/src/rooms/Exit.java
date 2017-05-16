346d437399ef9fdf6323017edecb9818507ee5b6
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-05-16 12:04:54 -0500

    Update Exit.java

--- Chad'sQuest/src/rooms/Exit.java
+++ Chad'sQuest/src/rooms/Exit.java
@@ -1,12 +1,21 @@
-
-
-
+//MAAS
+//Exit
+//5-19-17
 package rooms;
+
+import java.util.Scanner;
 
 public class Exit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void Exfil() 
+	{
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Chad walks down to the floor level of the office, passing all of his coworkers' offices on his way.");
+		System.out.print("\nHe can't help wondering what if he had done things differently.");
+		System.out.print("\nWhat if he had gone here, or done this?");
+		System.out.print("\n");
 
 	}
 
