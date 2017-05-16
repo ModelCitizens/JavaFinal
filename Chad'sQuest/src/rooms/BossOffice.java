aded927893b548840ac18b849031a7aade46aa95
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-05-16 12:04:32 -0500

    Update BossOffice.java

--- Chad'sQuest/src/rooms/BossOffice.java
+++ Chad'sQuest/src/rooms/BossOffice.java
@@ -1,10 +1,96 @@
+//MAAS
+//Brian's office
+//5-12-17
 package rooms;
+
+import java.util.Scanner;
 
 public class BossOffice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void Boroom()
+	{
+		String dialogueChoice;
+		String doorChoice;
+		String calmChoice;
+		int [] plaque = {2017,2018};
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("After going up th last flight of stairs Chad finds himself outside Brian's office.");
+		System.out.print("\nBrian is the spastic boss of Blue Ring. He often leaves to go on business trips leaving the staff to run themselves.");
+		System.out.print("\nNo use standing here, lets go!");
+		System.out.print("OPEN DOOR >>>");
+		doorChoice = input.nextLine();
+		 if(doorChoice.equals("OPEN DOOR")||doorChoice.equals("open door"))
+		 {
+			 System.out.print("In the office is a plaque that reads " + plaque[0] + " along with a picture of the ribbon cutting ceremony.");
+			 System.out.print("\nChad looks around the office in search of a key card, but other than Brian's strange souvenirs nothing can be found.");
+			 System.out.print("\nWell it looks like there isn't one here, guess we gotta go bac-");
+			 System.out.print("\nSuddenly John walks out of Brian's personal bathroom, shutting the door behind him after pulling his cart out.");
+			 System.out.print("\nOh boy, so John definitely has a key card on him, but the thing is he's rather tiresome to speak to.");
+			 System.out.print("\nSo I'm gonna lay it out for you, we can either talk to John and attempt to get a key card from him, or we can go all the way back to the original hallway and go right.");
+			 System.out.print("\nIt's up to you, but personally I think it'd be quicker just to go back.");
+			 System.out.print("\nASK JOHN or TURN BACK? >>>");
+			 dialogueChoice = input.nextLine();
+			 if(dialogueChoice.equals("ASK JOHN")||dialogueChoice.equals("ask john"))
+			 {
+				 System.out.print("\nThis oughta be good. HEY JOHN! C'mere I gotta ask you something.");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nI said c'mere.");
+				 System.out.print("\nJohn: Okay!");
+				 System.out.print("\nJohn walks over to stand in front of Chad.");
+				 System.out.print("\nDo you have a key card that Chad can borrow for the night?");
+				 System.out.print("\nJohn: Yeeeah!");
+				 System.out.print("\nGreat, can he have it?");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nThe key card, the one you said you had.");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nYou said you had a spare key card, right?");
+				 System.out.print("\nJohn: Yeeeah!");
+				 System.out.print("\nAnd that Chad could use it, right?");
+				 System.out.print("\nJohn: Yeeeah!");
+				 System.out.print("\nSo then can you grab it for us?");
+				 System.out.print("\nJohn: Okay!");
+				 System.out.print("\n...");
+				 System.out.print("\n...");
+				 System.out.print("\n...");
+				 System.out.print("\nJohn?");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nWhy aren't you getting the key card?");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nI'VE HAD ENOUGH!!! Why do you always talk in a circle!? Are you some sort of demented magic 8-ball!?");
+				 System.out.print("\nJohn: Ooooowhat?");
+				 System.out.print("\nI'm gonna kill'em. I'm gonna do it. I'm actually gonna kill'em I swear.");
+				 System.out.print("\nKO or LET BE? >>>");
+				 calmChoice = input.nextLine();
+				 if(calmChoice.equals("KO")||calmChoice.equals("ko"))
+				 {
+					 System.out.print("\nChad grabs John on the shoulder and smiles.");
+					 System.out.print("\nJohn looks at him with a puzzled, look opens his mouth to reply, aaaand is then immediately knocked out by a sucker punch along the jaw.");
+					 System.out.print("\nTHANK YOU!!! Let's grab the key card and get out before he starts talking again.");
+					 System.out.print("\nChad looks through John's cart locating the key card and leaving.");
+					 Exit.Exfil();
+				 }
+				 if(dialogueChoice.equals("LET BE")||dialogueChoice.equals("let be"))
+				 {
+					 System.out.print("\nJohn: Ooowha-");
+					 System.out.print("\nSuddenly a large hole appears and a giant hand flies out smashing John, and the narrator begins laughing.");
+					 System.out.print("\nThat's what we call breaking the fourth wall boy! Now lets see about that key card.");
+					 System.out.print("\nThe hand grabs the cart and rummages through it for a few minutes before finding a small piece of plastic.");
+					 System.out.print("\nHere it is. There you go Chad.");
+					 System.out.print("\nThe hand deposits the key card into Chad's hands before fading away.");
+					 System.out.print("\nTime to leave!");
+					 Exit.Exfil();
+				 }
+				 
+			 }
+			 if(dialogueChoice.equals("TURN BACK")||dialogueChoice.equals("turn back"))
+			 {
+				 System.out.print("Chad leaves Brian's office and goes all the way back to the original hallway, this time opening the right door.");
+				 RightRoom.RightHall();
+			 }
+			 
+		 }
 	}
 
 }
